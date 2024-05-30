b6c895f64f14daca5cab365894928ceccfa52eae
aadijain725 <aadijain725@gmail.com>
2024-05-30 09:34:26 +0530

    finalising registration pages and logic

--- src/Registration/researcher-register.jsx
+++ src/Registration/researcher-register.jsx
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./researcher-register.css";
 import Select from "react-select";
-import { useNavigate } from "react-router-dom"; // Use useNavigate for redirection
 
 const ResearcherRegistration = ({ registerCallback }) => {
-  const navigate = useNavigate(); 
-
   const [highestDegreeObtained, setHighestDegreeObtained] = useState("");
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [resume, setResume] = useState(null);
   const [instituteName, setInstituteName] = useState("");
-  const [instituteAddress, setInstituteAddress] = useState("");
   const [instituteType, setInstituteType] = useState("");
 
   const InstituteOptions = [
@@ -22,18 +17,13 @@ const ResearcherRegistration = ({ registerCallback }) => {
 
   const handleInstituteNameChange = (e) => {
     setInstituteName(e.target.value);
-  }
+  };
 
   const handleInstituteTypeChange = (selectedOption) => {
     console.log("Selected option: ", selectedOption);
     setInstituteType(selectedOption);
     // setSelectedOption(selectedOption);
   };
-
-  const handleInstituteAddressChange = (e) => {
-    setInstituteAddress(e.target.value);
-  };
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -43,22 +33,18 @@ const ResearcherRegistration = ({ registerCallback }) => {
       highest_degree_earned: highestDegreeObtained,
       institution_name: instituteName,
       institution_type: instituteType.value,
-      institution_address: instituteAddress,
+      // institution_address: instituteAddress,
     };
     if (areasOfStudy.length > 0) {
       fieldDetails["areas_of_study"] = areasOfStudy;
     }
 
     console.log("Field details: ", fieldDetails);
-    registerCallback(fieldDetails)
+    registerCallback(fieldDetails);
   };
 
   const handleChange = (selectedOptions) => {
     setSelectedOptions(selectedOptions);
-  };
-
-  const handleFileChange = (e) => {
-    setResume(e.target.files[0]);
   };
 
   const options = [
@@ -71,13 +57,34 @@ const ResearcherRegistration = ({ registerCallback }) => {
     { value: "urban_planning_and_design", label: "Urban Planning and Design" },
     { value: "climate_science", label: "Climate Science" },
     { value: "remote_sensing", label: "Remote Sensing" },
-    { value: "geographic_information_system", label: "Geographic Information System" },
-    { value: "energy_efficiency_and_urban_sustainability", label: "Energy Efficiency and Urban Sustainability" },
-    { value: "urban_transportation_and_mobility", label: "Urban Transportation and Mobility" },
-    { value: "urban_data_science_and_modelling", label: "Urban Data Science and Modelling" },
-    { value: "urban_disaster_risk_reduction", label: "Urban Disaster Risk Reduction" },
-    { value: "urban_climate_adaptation_planning", label: "Urban Climate Adaptation Planning" },
-    { value: "urban_carbon_footprint_assessment", label: "Urban Carbon Footprint Assessment" },
+    {
+      value: "geographic_information_system",
+      label: "Geographic Information System",
+    },
+    {
+      value: "energy_efficiency_and_urban_sustainability",
+      label: "Energy Efficiency and Urban Sustainability",
+    },
+    {
+      value: "urban_transportation_and_mobility",
+      label: "Urban Transportation and Mobility",
+    },
+    {
+      value: "urban_data_science_and_modelling",
+      label: "Urban Data Science and Modelling",
+    },
+    {
+      value: "urban_disaster_risk_reduction",
+      label: "Urban Disaster Risk Reduction",
+    },
+    {
+      value: "urban_climate_adaptation_planning",
+      label: "Urban Climate Adaptation Planning",
+    },
+    {
+      value: "urban_carbon_footprint_assessment",
+      label: "Urban Carbon Footprint Assessment",
+    },
     { value: "others", label: "Others" },
   ];
 
@@ -108,7 +115,7 @@ const ResearcherRegistration = ({ registerCallback }) => {
         />
       </div>
       <div className="form-group">
-        <label htmlFor="InstituteType">Institute Type:</label>
+        <label htmlFor="InstituteType">Organization Type:</label>
         <Select
           options={InstituteOptions}
           value={instituteType}
@@ -118,7 +125,7 @@ const ResearcherRegistration = ({ registerCallback }) => {
         />
       </div>
       <div className="form-group">
-        <label htmlFor="Name">Institution Name</label>
+        <label htmlFor="Name">Organization Name</label>
         <input
           type="text"
           placeholder="Name of Institute"
@@ -127,7 +134,7 @@ const ResearcherRegistration = ({ registerCallback }) => {
           className="input_main"
         />
       </div>
-      <div className="form-group">
+      {/* <div className="form-group">
         <label htmlFor="instituteAddress">Institute Address:</label>
         <textarea
           id="institute Address"
@@ -135,7 +142,7 @@ const ResearcherRegistration = ({ registerCallback }) => {
           onChange={handleInstituteAddressChange}
           required
         />
-      </div>
+      </div> */}
       <div className="form-group">
         <div className="submit-container">
           <button type="submit" className="submit">
@@ -145,6 +152,6 @@ const ResearcherRegistration = ({ registerCallback }) => {
       </div>
     </form>
   );
-}
+};
 
 export default ResearcherRegistration;
