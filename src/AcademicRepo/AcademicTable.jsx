8864d8784e73d205375de8206f3fbe10d4706f6c
aadijain725 <aadijain725@gmail.com>
2024-06-02 22:03:28 +0530

    added fetching logic from API for Academic Table

--- src/AcademicRepo/AcademicTable.jsx
+++ src/AcademicRepo/AcademicTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Collapse from "@mui/material/Collapse";
 import Table from "@mui/material/Table";
@@ -9,18 +9,22 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import IconButton from "@mui/material/IconButton";
 
 import {
-  MDBContainer, 
-  MDBTable, 
-  MDBTableHead, 
+  MDBContainer,
+  MDBTable,
+  MDBTableHead,
   MDBTableBody,
   MDBIcon,
-  MDBCollapse, 
-} from 'mdb-react-ui-kit';
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+
+function getFilename(path) {
+  return path.split("/").pop();
+}
 
 function createData(
   id,
@@ -46,11 +50,11 @@ function createData(
     upload_date,
     content,
     abstract,
-    keywords
+    keywords,
   };
 }
 
-const rows = [
+const backup_rows = [
   createData(
     "1",
     "Thesis on Quantum Computing",
@@ -76,11 +80,33 @@ const rows = [
     "This is the content of the Deep Learning Advances thesis in PDF format.",
     "An in-depth look at the latest advancements in deep learning and their applications in various fields.",
     "Deep Learning, Neural Networks, AI, Machine Learning"
-  )
+  ),
 ];
 
 export default function CollapsibleTable() {
   const [expandedRow, setExpandedRow] = useState(null);
+
+  const [rows, setRows] = useState([backup_rows]);
+
+  const fetchAllPapers = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/v1/file/all");
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Data:", data);
+      setRows(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPapers();
+  }, []);
 
   const toggleRowDetails = (index) => {
     setExpandedRow(expandedRow === index ? null : index);
@@ -102,70 +128,107 @@ export default function CollapsibleTable() {
               <th></th>
             </tr>
           </MDBTableHead>
-          <MDBTableBody style={{ verticalAlign: 'middle' }}>
+          <MDBTableBody style={{ verticalAlign: "middle" }}>
             {rows.map((row, index) => (
               <React.Fragment key={index}>
-              <tr>
-                <td>
-                <p className="fw-normal mb-1">{row.title}</p>
-                </td>
-                <td>
-                  <p className="fw-normal mb-1">{row.type}</p>
-                </td>
-                <td>
-                  <p className="fw-normal mb-1">{row.authors}</p>
-                </td>
-                <td>
-                  <p className="fw-normal mb-0">{row.degree_program}</p>
-                </td>
-                <td>
-                  <p className="fw-normal mb-0">{row.published_on}</p>
-                </td>
-                <td>
-                  <p className="fw-normal mb-0">{row.file_size}</p>
-                </td>
-                <td>
-                  <p className="fw-normal mb-0">{row.upload_date}</p>
-                </td>
-                <td>
-                <MDBIcon
-                    fas
-                    icon={expandedRow === row.id ? 'caret-up' : 'caret-down'}
-                    onClick={() => toggleRowDetails(row.id)}
-                    style={{ cursor: 'pointer', color: '#0d6efd', fontSize: '1.5rem' }}
-                  />
-                </td>
-              </tr>
-              <tr style={{ display: expandedRow === row.id ? 'table-row' : 'none' }}>
-                <td colSpan={8}>
-                  <MDBCollapse open={expandedRow === row.id}>
-                    <div className="p-3">
-                      <p>
-                        <strong>Ttile:</strong> {row.title}
-                      </p>
-                      <p>
-                        <strong>Authors:</strong> {row.authors}
-                      </p>
-                      <p>
-                        <strong>Degree/Program:</strong> {row.degree_program}
-                      </p>
-                      <p>
-                        <strong>Publicatio Date:</strong> {row.published_on}
-                      </p>
-                      <p>
-                        <strong>Abstract:</strong> {row.abstract}
-                      </p>                      
-                      <p>
-                        <strong>Keywords:</strong> {row.keywords}
-                      </p>
-                      <p>
-                        <strong>Content:</strong> {row.content}
-                      </p>
-                    </div>
-                  </MDBCollapse>
-                </td>
-              </tr>
-            </React.Fragment>
+                <tr>
+                  <td>
+                    <p className="fw-normal mb-1">{row.title}</p>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-1">{row.fileType}</p>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-1">{row.author}</p>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-0">{row.degree_program}</p>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-0">{new Date(row.publication_date).toDateString()}</p>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-0">{row.fileSize}</p>
+                  </td>
+                  <td>
+                    <p className="fw-normal mb-0">{new Date(row.uploadDate).toDateString()}</p>
+                  </td>
+                  <td>
+                    <MDBIcon
+                      fas
+                      icon={expandedRow === row.id ? "caret-up" : "caret-down"}
+                      onClick={() => toggleRowDetails(row.id)}
+                      style={{
+                        cursor: "pointer",
+                        color: "#0d6efd",
+                        fontSize: "1.5rem",
+                      }}
+                    />
+                  </td>
+                </tr>
+                <tr
+                  style={{
+                    display: expandedRow === row.id ? "table-row" : "none",
+                  }}
+                >
+                  <td colSpan={8}>
+                    <MDBCollapse open={expandedRow === row.id}>
+                      <div className="p-3">
+                        <p>
+                          <strong>Ttile:</strong> {row.title}
+                        </p>
+                        <p>
+                          <strong>Authors:</strong> {row.author}
+                        </p>
+                        <p>
+                          <strong>Degree/Program:</strong> {row.degree_program}
+                        </p>
+                        <p>
+                          <strong>Publication Date:</strong>{" "}
+                          {new Date(row.publication_date).toDateString()}
+                        </p>
+                        <p>
+                          <strong>Abstract:</strong> {row.abstract}
+                        </p>
+                        <p>
+                          <strong>Keywords:</strong> {row.keywords}
+                        </p>
+
+                        <p>
+                          <strong>Thesis Document:</strong>{" "}
+                          {row.filePath && (
+                            <a
+                              href={`http://localhost:3001/static/${getFilename(
+                                row.filePath
+                              )}`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Thesis
+                            </a>
+                          )}
+                        </p>
+                        <p>
+                          {row.synopsisFilePath && (
+                            <>
+                              <strong>Synopsis Document:</strong>{" "}
+                              <a
+                                href={`http://localhost:3001/static/${getFilename(
+                                  row.synopsisFilePath
+                                )}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                View Synopsis
+                              </a>
+                            </>
+                          )}
+                        </p>
+                      </div>
+                    </MDBCollapse>
+                  </td>
+                </tr>
+              </React.Fragment>
             ))}
           </MDBTableBody>
         </MDBTable>
